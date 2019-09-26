9956355ebea2d43823651cb3d654a8bda1d9d36f
MarcosGaming <40284094@live.napier.ac.uk>
2019-09-26 12:48:26 +0100

    Practical 2 exercise 10 left

--- Practical2/src/Application.java
+++ Practical2/src/Application.java
@@ -16,9 +16,22 @@ public class Application {
 			AgentController rma = myContainer.createNewAgent("rma","jade.tools.rma.rma", null);
 			rma.start();
 			// Now start your own BookBuyerAgent called buyer
-			String[] books = {"Java for Dummies"};
-			AgentController myAgent = myContainer.createNewAgent("buyer", BookBuyerAgent.class.getCanonicalName(), books);
+			String[] books = {"Java"};
+			AgentController myAgent = myContainer.createNewAgent("buyerA", BookBuyerAgent.class.getCanonicalName(), books);
 			myAgent.start();
+			AgentController buyerAgentB = myContainer.createNewAgent("buyerB", BookBuyerAgent.class.getCanonicalName(), books);
+			buyerAgentB.start();
+			// Start seller agents
+			AgentController sellerAgentA = myContainer.createNewAgent("sellerA", BookSellerAgent.class.getCanonicalName(), null);
+			sellerAgentA.start();
+			/*AgentController sellerAgentB = myContainer.createNewAgent("sellerB", BookSellerAgent.class.getCanonicalName(), null);
+			sellerAgentB.start();
+			AgentController sellerAgentC = myContainer.createNewAgent("sellerC", BookSellerAgent.class.getCanonicalName(), null);
+			sellerAgentC.start();
+			AgentController sellerAgentD = myContainer.createNewAgent("sellerD", BookSellerAgent.class.getCanonicalName(), null);
+			sellerAgentD.start();
+			AgentController sellerAgentE = myContainer.createNewAgent("sellerE", BookSellerAgent.class.getCanonicalName(), null);
+			sellerAgentE.start();*/
 		} catch(Exception e){
 			System.out.println("Exception starting agent: " + e.toString());
 		}
